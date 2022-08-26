fa69435ae92655d8fb9cbef3b3f6df0f4c85f7b9
ocassidydev <ocassidy@tcd.ie>
2022-08-26 09:19:43 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -25,7 +24,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:\n")
         
         sales_data = data_str.split(",")
 
@@ -61,7 +60,7 @@ def update_worksheet(data, worksheet):
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully.\n")
+    print(f"{worksheet.capitalize()} worksheet updated successfully.\n")
 
 def calculate_surplus_data(sales_row):
     """
@@ -97,16 +96,33 @@ def get_last_5_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 def main():
     """
     Run all program functions
     """
-    # data = get_sales_data()
-    # sales_data = [int(num) for num in data]
-    # update_worksheet(sales_data, "sales")
-    # new_surplus_data = calculate_surplus_data(sales_data)
-    # update_worksheet(new_surplus_data, "surplus")
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
     
 
 print("Welcome to Love Sandwiches Data Automation")
